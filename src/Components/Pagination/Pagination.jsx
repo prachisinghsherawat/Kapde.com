29151cadd8d9b17f00bb3eb39736d2368085e7df
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-26 18:52:11 +0530

    Pagination

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+export default function PaginationControlled({handleChange,page}) {
+
+  return (
+    <Stack className='PageBox' spacing={2}>
+      <Typography>Page: {page}</Typography>
+      <Pagination id='pagination' count={3} page={page} color='primary' onChange={handleChange} />
+    </Stack>
+  );
+}
